79ffc50f3f117a9ad99f7bdbc95fac11121337f7
tyamagis @ GCML <tyamagis@student.42tokyo.jp>
2022-01-17 00:47:54 +0900

    02/01 add test for sigaction()

--- 02/01_minitalk/test/cl_sigaction.c
+++ 02/01_minitalk/test/cl_sigaction.c
@@ -2,18 +2,21 @@
 #include <unistd.h>
 #include <stdio.h>
 
-/*
- * SIGACTION
- *
- * int	sigaction(int signum, const stcruct sigaction *act, struct sigaction *oldact);
- *
- * struct sigaction{
- * 		void		(*sa_handler)(int);
- * 		void		(*sa_sigaction)(int, siginfo_t *, void *);
- * 		sigset_t	sa_mask;
- * 		int			sa_flags;
- * 		void		(*sa_restorer)(void);
- * 		}
- *
- *
- * */
+void	result(int signum)
+{
+	o
+
+int main(int ac, char **av)
+{
+	struct sigaction sa_old;
+	struct sigaction sa_new;
+
+	sa_new.sa_handler = result;
+	sigemptyset(&sa_new.sa_mask);
+	// sa_new.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa_new, &sa_old);
+
+	sigaction(SIGUSR1, &sa_old, 0);
+
+	return (0);
+}
